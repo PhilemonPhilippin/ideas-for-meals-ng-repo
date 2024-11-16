42ac6847b9fdd528727ae11ed078261c42a69cd9
Philemon Philippin <p_philippin@hotmail.com>
2024-11-16 13:18:08 +0100

    helvetica and garamond fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garamond: ["EB Garamond", "serif"],
+        helvetica: ["Helvetica", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
