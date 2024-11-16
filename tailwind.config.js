9bd0552d6b4ffef89f6abc564fcf411c5d03c9ec
Philemon Philippin <p_philippin@hotmail.com>
2024-11-16 13:21:33 +0100

    custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      colors: {
+        "light-black": "var(--light-black)",
+        "dark-blue": "var(--dark-blue)",
+        "light-blue": "var(--light-blue)",
+        "lighter-blue": "var(--lighter-blue)",
+        "light-grey": "var(--light-grey)",
+        "dark-grey": "var(--dark-grey)",
+        "light-red": "var(--light-red)",
+      },
       fontFamily: {
         garamond: ["EB Garamond", "serif"],
         helvetica: ["Helvetica", "sans-serif"],
