aea6df0996b0d83f5440f8a3af0e6940af92eef0
Philemon Philippin <p_philippin@hotmail.com>
2024-11-27 12:08:12 +0100

    modified to call API with authorization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authHttpInterceptorFn } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,8 +12,22 @@ export const appConfig: ApplicationConfig = {
       clientId: 'DxyPUtOYEV9ljMZsSVbeL1iOOctVoIdZ',
       authorizationParams: {
         redirect_uri: window.location.origin,
+        audience: 'https://localhost:7273/api',
+      },
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: 'https://localhost:7273/api/*',
+            tokenOptions: {
+              authorizationParams: {
+                audience: 'https://localhost:7273/api',
+              },
+            },
+          },
+        ],
       },
     }),
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
